aea30b67a1b6daa3151afcbe41393029f2850ed0
Florian Hönicke <hoenicke.florian@gmail.com>
2022-01-03 20:48:07 +0100

    fix: blob is none

--- executor.py
+++ executor.py
@@ -52,7 +52,7 @@ class ImagePreprocessor(Executor):
     def preprocess(self, docs: DocumentArray, **_) -> Optional[DocumentArray]:
         """Preprocess docs."""
         for d in docs:
-            if d.uri and not d.blob:
+            if d.uri and d.blob is None:
                 d.load_uri_to_image_blob()
         self._reshape(docs)
         self._normalize(docs)
